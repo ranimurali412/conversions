cb7f76421b81c60076d4478994be91c03158242b
Rani Murali <rmurali@hks.harvard.edu>
2021-01-14 12:16:15 -0800

    completing example 2 exercise

--- example2/convert.py
+++ example2/convert.py
@@ -4,12 +4,21 @@ import json
 import csv
 
 # use json.load to load input.json. load it into a variable called 'data'
-
+with open('input.json') as f:
+	data = json.load(f)
 # use open to open 'output.csv' in write mode
-# use csv.writer to create writer using the opened file
-# write the header of the csv
+with open('output.csv', 'w') as f:
+	# use csv.writer to create writer using the opened file
+	writer = csv.writer(f)
+	# write the header of the csv
+	writer.writerow(['id', 'value'])
+	# loop through the top level children data['children']
+	for row in data['children']:
+		# use writer.writerow to write one row at a time with the name and budget
+		# - prepend 'flare.other.' to each name
+		# - remove any spaces in the name
+		id2 = 'flare.other.' + row['name'].replace(' ', '')
 
-# loop through the top level children data['children']
-# use writer.writerow to write one row at a time with the name and budget
-# - prepend 'flare.other.' to each name
-# - remove any spaces in the name
+		budget = row['data']['budget'].replace('$', '').replace(',', '')
+
+		writer.writerow([id2, budget])
